9d04a41a2b482573e03f76c82df0632e1fa28959
user <feruzatulaganova027@gmail.com>
2025-05-10 08:58:06 +0500

    start project

---
+++ keyboards.py
@@ -0,0 +1,7 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
+
+web_app = WebAppInfo(url="https://github.com/YuldashevIslombek/p2_web_app.github.io/")
+web_app_btn = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text="Web App", web_app=web_app)]
+])
+
